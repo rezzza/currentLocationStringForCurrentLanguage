e999dc5ad73c26517a9d8958b26c9f3995e98658
Sjors Provoost <sjors@sprovoost.nl>
2012-09-14 10:48:14 +0200

    Use language preference. Thanks @tommecpe

--- LocalizedCurrentLocation.m
+++ LocalizedCurrentLocation.m
@@ -42,7 +42,8 @@
                                                nil];
     
     NSString *localizedString;
-    NSString *currentLanguageCode = [[NSLocale currentLocale] objectForKey: NSLocaleLanguageCode];
+    // NSString *currentLanguageCode = [[NSLocale currentLocale] objectForKey: NSLocaleLanguageCode];
+    NSString *currentLanguageCode = [[NSLocale preferredLanguages] objectAtIndex:0];
     
     if ([localizedStringDictionary valueForKey:currentLanguageCode]) {
         localizedString = [NSString stringWithString:[localizedStringDictionary valueForKey:currentLanguageCode]];
